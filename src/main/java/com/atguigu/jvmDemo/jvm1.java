629b72f002d1545c68936a7cfb8953469c710c7d
gyd <2506880031@qq.com>
2019-12-18 18:03:51 +0800

    jvm

---
+++ src/main/java/com/atguigu/jvmDemo/jvm1.java
@@ -0,0 +1,16 @@
+package com.atguigu.jvmDemo;
+
+public class jvm1 {
+    //java虚拟机
+    public static void main(String[] args) {
+        jvm1 jvm1 = new jvm1();
+        System.out.println(jvm1.getClass().getClassLoader().getParent().getParent());
+        System.out.println(jvm1.getClass().getClassLoader().getParent());
+        System.out.println(jvm1.getClass().getClassLoader());
+        a();
+    }
+
+    static void a() {
+        a();
+    }
+}
