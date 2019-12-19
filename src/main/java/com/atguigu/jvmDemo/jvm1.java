df5596a8ee4af88184205b22a133427a44324594
gyd <2506880031@qq.com>
2019-12-19 10:44:09 +0800

    temp

--- src/main/java/com/atguigu/jvmDemo/jvm1.java
+++ src/main/java/com/atguigu/jvmDemo/jvm1.java
@@ -4,13 +4,14 @@ public class jvm1 {
     //java虚拟机
     public static void main(String[] args) {
         jvm1 jvm1 = new jvm1();
-        System.out.println(jvm1.getClass().getClassLoader().getParent().getParent());
+       /* System.out.println(jvm1.getClass().getClassLoader().getParent().getParent());
         System.out.println(jvm1.getClass().getClassLoader().getParent());
-        System.out.println(jvm1.getClass().getClassLoader());
-        a();
+        System.out.println(jvm1.getClass().getClassLoader());*/
+        System.out.println("111111");
+      //  a();
     }
 
-    static void a() {
-        a();
-    }
+   // static void a() {
+      //  a();
+    //}
 }
